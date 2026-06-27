7f0bf9b6c113e8e9ac1743628af6b63b1589fe0b
dmsabado-eng <dmsabado@gmail.com>
2026-06-26 18:18:48 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int year1 = 1;
+    int year3 = 3;
+    int year5 = 5;
+    int year7 = 7;
+
+    cout << "Name: First Last" << endl;
+    cout << "Goals in Life: My goal is to keep learning, grow my skills, and build a successful future." << endl;
+    cout << "Current Interests: I am interested in technology, programming, and improving myself." << endl;
+    cout << "Dreams: My dream is to have a stable career and make my family proud." << endl;
+
+    cout << "In " << year1 << " year, I see myself improving my coding skills." << endl;
+    cout << "In " << year3 << " years, I see myself doing well in college and gaining experience." << endl;
+    cout << "In " << year5 << " years, I see myself working toward my career goals." << endl;
+    cout << "In " << year7 << " years, I see myself successful and stable in my future career." << endl;
+
+    return 0;
+}
